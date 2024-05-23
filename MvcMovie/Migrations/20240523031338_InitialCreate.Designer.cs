215ca45a625043c37d11a89d19cb35e17a94b8c2
laylark <laylan@gmail.com>
2024-05-22 22:15:26 -0500

    feat: add Rating field to movies table

--- MvcMovie/Migrations/20240523031338_InitialCreate.Designer.cs
+++ MvcMovie/Migrations/20240523031338_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20240507184600_InitialCreate")]
+    [Migration("20240523031338_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -30,6 +30,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
